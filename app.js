4348fc18cd63602084850b0fbcf651ba94bb1449
Tony Gorgioski <tonygorgioski@Tonys-MBP.modem>
2021-08-02 09:36:40 +1000

    added search functionality

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const audio = document.querySelectorAll('audio');
 const buttons = document.querySelectorAll('.btn');
 
+const container = document.querySelector('.container');
+const input = document.querySelector('input');
+
 const sounds = [...audio];
 
 ['click', 'ontouchstart'].forEach((evt) => {
@@ -13,5 +16,27 @@ const sounds = [...audio];
       });
       if (!playing) sound.play();
     });
+  });
+});
+
+input.addEventListener('keyup', (e) => {
+  container.innerHTML = '';
+  let data = [...buttons];
+  let searchResults = data.filter((item) => {
+    const regex = new RegExp(input.value, 'gi');
+    return item.innerText.match(regex);
+  });
+  if (searchResults.length === 0) {
+    container.innerText = `We'll be right back...`;
+    return;
+  }
+  // container.innerText = '';
+  // // paint the search results, starting the cycle again
+  // paintElements(searchResults);
+  searchResults.forEach((result) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.append(result);
+    container.append(card);
   });
 });
