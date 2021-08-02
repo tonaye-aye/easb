623e6f5aebce23ba71f15a82fc40d050e9514efa
Tony Gorgioski <tonygorgioski@Tonys-MBP.modem>
2021-08-02 13:28:20 +1000

    added search functionality

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const audio = document.querySelectorAll('audio');
 const buttons = document.querySelectorAll('.btn');
+const sounds = [...audio];
+const buttonsArray = [...buttons];
 
 const container = document.querySelector('.container');
 const input = document.querySelector('input');
 const clear = document.querySelector('.clear');
 
-const sounds = [...audio];
+const clearButton = () => {
+  if (input.value) {
+    clear.style.visibility = `visible`;
+    clear.classList.add('close');
+  } else {
+    clear.style.visibility = `hidden`;
+    clear.classList.remove('close');
+  }
+};
+
+const createSoundCards = (data) => {
+  data.forEach((result) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.append(result);
+    container.append(card);
+  });
+};
 
 ['click', 'ontouchstart'].forEach((evt) => {
   buttons.forEach((button) => {
@@ -22,15 +41,11 @@ const sounds = [...audio];
 
 input.addEventListener('keyup', (e) => {
   // toggle clear 'x' icon
-  input.value
-    ? (clear.style.visibility = `visible`)
-    : (clear.style.visibility = `hidden`);
+  clearButton();
   // clear the search container
   container.innerHTML = '';
-  // get all of the sound buttons and save to data array
-  let data = [...buttons];
   // filter the data by matching the search keyword
-  let searchResults = data.filter((item) => {
+  let searchResults = buttonsArray.filter((item) => {
     const regex = new RegExp(input.value, 'gi');
     return item.innerText.match(regex);
   });
@@ -43,10 +58,13 @@ input.addEventListener('keyup', (e) => {
     return;
   }
   // otherwise if there is a search result, lets paint in the buttons
-  searchResults.forEach((result) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.append(result);
-    container.append(card);
-  });
+  createSoundCards(searchResults);
+});
+
+clear.addEventListener('click', () => {
+  container.innerHTML = '';
+  input.value = '';
+  input.focus();
+  clearButton();
+  createSoundCards(buttonsArray);
 });
