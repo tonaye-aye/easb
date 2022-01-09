4b1afca53702e029112c22c26923432780dfd4d0
Tony Gorgioski <tony.gorgioski@gmail.com>
2022-01-09 22:56:18 +1100

    Added tailwindcss to soundboard buttons

--- components/SoundBoard.js
+++ components/SoundBoard.js
@@ -5,6 +5,7 @@ import Input from '@components/input';
 
 export default function SoundBoard() {
   const soundRefs = useRef([]);
+  const buttonRefs = useRef([]);
   const inputRef = useRef(null);
 
   const [playing, setPlaying] = useState(false);
@@ -29,7 +30,8 @@ export default function SoundBoard() {
     }
   }, [searchInput]);
 
-  const handleSound = (id) => {
+  const handleSound = (e, id) => {
+    e.preventDefault();
     if (playing) return;
     soundRefs.current[id].play();
     setPlaying(true);
@@ -49,15 +51,23 @@ export default function SoundBoard() {
         <div className="container">
           {filteredSounds.map(({ id, title, src }) => (
             <div key={id} className="card">
-              <div className="btn no-select" onClick={() => handleSound(id)}>
+              <button
+                className="w-full no-select rounded-md text-3xl text-white px-4 py-8 text-center bg-pink-700 transition ease-in-out duration-300 hover:scale-105"
+                ref={(el) => buttonRefs.current.push(el)}
+                onClick={(e) => handleSound(e, id)}
+                type="button"
+              >
                 {title}
-              </div>
+              </button>
               <audio id={id} ref={(el) => soundRefs.current.push(el)} src={src}>
                 Your browser does not support the
                 <code>audio</code> element.
               </audio>
             </div>
           ))}
+          {filteredSounds.length === 0 && (
+            <div className="message">No results found.</div>
+          )}
         </div>
       </main>
     </>
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
       </Head>
 
       <Header />
-
       <SoundBoard />
       <Footer />
     </div>
