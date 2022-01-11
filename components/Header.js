6c022182d67f57122ebacea2a0163548a0dc721c
Tony Gorgioski <tony.gorgioski@gmail.com>
2022-01-12 10:01:12 +1100

    Fixed header issue

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,13 @@ import headerImg from '../public/assets/header.png';
 
 export default function Header() {
   return (
-    <header className="w-full p-2 bg-yellow-400">
-      <div className="w-full md:max-w-5xl grid place-items-center">
-        <Image
-          src={headerImg}
-          width={300}
-          height={100}
-          alt="The Eric Andre Show"
-        />
-      </div>
+    <header className="flex w-full px-2 bg-yellow-400">
+      <Image
+        src={headerImg}
+        width={300}
+        height={100}
+        alt="The Eric Andre Show"
+      />
     </header>
   );
 }
