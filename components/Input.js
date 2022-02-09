4b07a9dcfc34247fbdeb1d494103a234ded2e6a5
Tony Gorgioski <tonyg@canva.com>
2022-02-09 18:31:55 +1100

    changes

--- components/Input.js
+++ components/Input.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react'
 
 export default function Input({
   setSearchInput,
@@ -6,26 +6,26 @@ export default function Input({
   sounds,
   disabledInput
 }) {
-  const inputRef = useRef(null);
-  const clearRef = useRef(null);
-  const [input, setInput] = useState('');
-  const [placeholder, setPlaceholder] = useState('Search...');
+  const inputRef = useRef(null)
+  const clearRef = useRef(null)
+  const [input, setInput] = useState('')
+  const [placeholder, setPlaceholder] = useState('Search...')
 
   useEffect(() => {
     if (inputRef.current) {
-      inputRef.current.focus();
+      inputRef.current.focus()
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
     const handleKeyDown = (e) => {
-      if (document.activeElement === inputRef.current) return;
+      if (document.activeElement === inputRef.current) return
       if (e.key === '/') {
-        inputRef.current.focus();
-        setInput('');
-        setPlaceholder('Search...');
+        inputRef.current.focus()
+        setInput('')
+        setPlaceholder('Search...')
       }
-    };
+    }
 
     const handleClickOutside = (e) => {
       if (
@@ -33,47 +33,47 @@ export default function Input({
         !inputRef.current.contains(e.target) &&
         !clearRef.current.contains(e.target)
       ) {
-        inputRef.current.blur();
-        setPlaceholder("Press '/' to focus...");
+        inputRef.current.blur()
+        setPlaceholder('Search here...')
       } else {
-        setPlaceholder('Search...');
+        setPlaceholder('Here we go!')
       }
-    };
+    }
 
-    document.addEventListener('keydown', handleKeyDown);
-    document.addEventListener('click', (e) => handleClickOutside(e));
+    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('click', (e) => handleClickOutside(e))
 
     return function cleanup() {
-      document.removeEventListener('keydown', handleKeyDown);
-      document.removeEventListener('click', (e) => handleClickOutside(e));
-    };
-  });
+      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('click', (e) => handleClickOutside(e))
+    }
+  })
 
   useEffect(() => {
     if (input !== '') {
-      clearRef.current.style.visibility = 'visible';
-      clearRef.current.style.pointerEvents = 'auto';
+      clearRef.current.style.visibility = 'visible'
+      clearRef.current.style.pointerEvents = 'auto'
     } else {
-      clearRef.current.style.visibility = 'hidden';
-      clearRef.current.style.pointerEvents = 'none';
+      clearRef.current.style.visibility = 'hidden'
+      clearRef.current.style.pointerEvents = 'none'
     }
-  }, [input]);
+  }, [input])
 
   const handleSearch = (value) => {
     let newValue = value
       .replace(/\\+/, '')
       .replace(/\/+/, '')
       .replace(/\[+/, '')
-      .replace(/\]+/, '');
-    setInput(newValue);
-    setSearchInput(newValue);
-  };
+      .replace(/\]+/, '')
+    setInput(newValue)
+    setSearchInput(newValue)
+  }
 
   const handleClear = () => {
-    inputRef.current.focus();
-    setInput('');
-    setFilteredSounds(sounds);
-  };
+    inputRef.current.focus()
+    setInput('')
+    setFilteredSounds(sounds)
+  }
 
   return (
     <div className="w-full sticky top-0 z-10 bg-stone-900 border-b-2 border-yellow-400">
@@ -96,5 +96,5 @@ export default function Input({
         </div>
       </form>
     </div>
-  );
+  )
 }
