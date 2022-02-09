afc414fb49fc847bead5f1d4eb7553dca6ab1eda
Tony Gorgioski <tonyg@canva.com>
2022-02-09 18:10:20 +1100

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,27 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#121212" />
+
+          <meta name="twitter:url" content="https://ericandre.app" />
+          <meta name="twitter:title" content="The Eric Andre Show Soundboard" />
+          <meta
+            name="twitter:description"
+            content="The crazy and wild sounds of Eric Andre featuring Hannibal Burres"
+          />
+          <meta name="twitter:creator" content="@im_tones" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="The Eric Andre Show Soundboard" />
+          <meta
+            property="og:description"
+            content="The crazy and wild sounds of Eric Andre featuring Hannibal Burres"
+          />
+          <meta property="og:site_name" content="Eric Andre" />
+          <meta property="og:url" content="https://ericandre.app" />
+          <meta
+            property="og:image"
+            content="https://ericandre.app/icons/icon-192x192.png"
+          />
         </Head>
         <body>
           <Main />
