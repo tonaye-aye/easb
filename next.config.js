08ee11eb06edf6889507909a8f6800fd9d7a3908
Tony Gorgioski <tonyg@canva.com>
2022-02-09 17:34:36 +1100

    fixed PWA config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -6,7 +7,8 @@ module.exports = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
     // disdable PWA in dev envronment
     disable: process.env.NODE_ENV === 'development'
   },
